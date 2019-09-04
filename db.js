93c9396f5ec6e2906a5221134a48fd8b14a7dbf8
ThelsK <github@thels.nl>
2019-09-04 16:00:34 +0200

    Prevent database from resetting

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const databaseUrl = process.env.DATABASE_URL ||
 const db = new Sequelize(databaseUrl)
 
 db
-  .sync({ force: true })
+  .sync()
   .then(() => console.log("Database schema updated"))
   .catch(console.error)
 
